e573291f6679a71d140d7fc6ab642503b02f9e78
Jiheon Lee <wlgjsdl0@gmail.com>
2021-04-09 23:20:30 +0900

    Add mainpage routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+  res.render('mainpage/index');
+});
+
+router.get('/introduce', (req, res, next) => {
+  res.render('mainpage/introduce');
 });
 
 module.exports = router;
