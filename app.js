43d7d5319787a0dd672a9d4f0f76b1d3c2cf13b9
Jiheon Lee <wlgjsdl0@gmail.com>
2021-04-10 22:16:15 +0900

    Add eslint, prettier, express-session, moment package

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const session = require('express-session');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const teachersRouter = require('./routes/teachers');
 
 const app = express();
 
@@ -18,9 +19,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  key: 'sid',
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 60*60*1000
+  }
+}));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/teachers', teachersRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
